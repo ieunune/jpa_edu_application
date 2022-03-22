eeb1f292f9c94d648d88447a58f53417aef2a27f
ieunune <50124623+ieunune@users.noreply.github.com>
2022-03-22 22:52:11 +0900

    기본 설정 구성 확인 Next : H2 DataBase initializing

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 롬복 적용 확인용 Hello 클래스
+ */
+@Getter @Setter
+public class Hello {
+    private String data;
+}
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+//		롬복 적용 확인
+//		Hello hello = new Hello();
+//		hello.setData("Hello");
+//		System.out.println(" get : " + hello.getData());
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
