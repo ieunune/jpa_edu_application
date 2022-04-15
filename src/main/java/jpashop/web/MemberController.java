cb4025c9de2c06725428f31f38cd29cd95f26c4d
ieunune <50124623+ieunune@users.noreply.github.com>
2022-04-15 23:05:37 +0900

    MemberController : 회원가입 기능 개발

---
+++ src/main/java/jpashop/web/MemberController.java
@@ -0,0 +1,11 @@
+package jpashop.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    // 작성 진행 중
+}
