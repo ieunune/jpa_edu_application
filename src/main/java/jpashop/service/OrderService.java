faff69dec63618ae90120aa88377174b54d63261
ieunune <50124623+ieunune@users.noreply.github.com>
2022-04-16 22:50:32 +0900

    실전! 스프링 부트와 JPA 활용 1 - 웹 애플리케이션 개발 강의 수료.

--- src/main/java/jpashop/service/OrderService.java
+++ src/main/java/jpashop/service/OrderService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -46,5 +48,7 @@ public class OrderService {
         order.cancel();
     }
 
-    // public List<Order> ~~
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
 }
